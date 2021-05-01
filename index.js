29616917d2e25cc45f3e123d39882f61c5aab2ee
dhxl50 <dhxl50@gmail.com>
2021-05-01 03:40:20 +0000

    Function Practice

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const title = document.getElementById("title");
-title.style.color = "blue";
-document.title = "modified";
+const title = document.querySelector("#title");
+const CLICKED_CLASS = "clicked"
+
+function handleClick(){
+	
+	title.classList.toggle(CLICKED_CLASS);
+}
+function handleMouseOver(event){
+	const color = title.style.color;
+	title.style.color = "blue";
+}
+
+function handleMouseLeave(event){
+	const color = title.style.color;
+	title.style.color = "black";
+}
+
+function init(){
+	//title.addEventListener("mouseover",handleMouseOver);
+	//title.addEventListener("mouseleave",handleMouseLeave);
+	title.addEventListener("click",handleClick);
+}
+
+init();
